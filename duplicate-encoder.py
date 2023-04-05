ded76c745a51e524695e29eed67c5ef2f337a218
Chaz <chaz@chazs-mbp.lan>
2023-04-05 07:16:22 -0700

    new challenges

--- duplicate-encoder.py
+++ duplicate-encoder.py
@@ -1,15 +1,22 @@
+"""https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/python"""
+
 def duplicate_encode(word):
+    nstr = ''
     d = {}
+    word = word.lower()
     for i in word:
         if i in d:
             d[i] += 1
         else:
             d[i] = 1
-    for k, v in d.items():
-        if v > 1:
-            print(k)
 
-
+    for i in word:
+        if d[i] == 1:
+            nstr += '('
+        else:
+            nstr += ')'
+    return nstr
 
 
 print(duplicate_encode("recede"))
+print(duplicate_encode("Success"))
